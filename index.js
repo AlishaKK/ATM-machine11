1749f40f9c0e712f4dec2142a6532f0c79098cc4
AlishaKK <162887724+AlishaKK@users.noreply.github.com>
2024-04-03 18:28:59 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let myBalance = 10000;
+let myPin = 12345;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "enter your pin",
+        type: "number",
+    },
+]);
+if (pinAnswer.pin === myPin) {
+    console.log("Correct Pin Code!");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "please select option",
+            type: "list",
+            choices: ["withdraw", "check balance"],
+        },
+    ]);
+    if (operationAns.operation === "withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "enter your amount",
+                type: "number",
+            },
+        ]);
+        if (amountAns.amount <= myBalance) {
+            myBalance -= amountAns.amount;
+            console.log(`Your remaining balance is ${myBalance}`);
+        }
+        else {
+            console.log("You have insufficient balance");
+        }
+    }
+    else if (operationAns.operation === "check balance") {
+        console.log(`Your balance is: ${myBalance}`);
+    }
+}
+else {
+    console.log("Wrong Pin Code!");
+}
